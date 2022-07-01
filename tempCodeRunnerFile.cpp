2ec9e6b9b47c8152a7f9a4417c8ef01ce9e936fc
Priyadarshan <priyadarshankumar926@gmail.com>
2022-07-01 17:52:14 +0530

    ignored

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int p=100;
+    if(p>20)
+    {
+        if(p<20)
+        {
+            cout<<"coding";
+        }
+    }
+        else
+        {
+            cout<<"ninjas";
+        }
+    }
